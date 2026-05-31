3f03945f58a173e6905fc4a1d6ea7aa439c8a046
Terry <terrylie0215@gmail.com>
2026-05-31 17:19:01 +0800

    Update firebase-admin.js

--- api/firebase-admin.js
+++ api/firebase-admin.js
@@ -1,8 +1,7 @@
-const admin = require('firebase-admin');
+import admin from 'firebase-admin';
 
 if (!admin.apps.length) {
   try {
-    // 請在 Vercel 設定 FIREBASE_SERVICE_ACCOUNT，內容為壓縮成一行的 JSON 字串
     const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
     admin.initializeApp({
       credential: admin.credential.cert(serviceAccount)
@@ -13,4 +12,4 @@ if (!admin.apps.length) {
 }
 
 const db = admin.apps.length ? admin.firestore() : null;
-module.exports = { admin, db };
+export { admin, db };
